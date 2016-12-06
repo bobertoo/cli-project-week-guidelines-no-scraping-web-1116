263dadcd4659b2b71825cccaec241496bdcdba23
Clinton Nguyen <clinton.ngyn@gmail.com>
2016-12-06 16:23:59 -0500

    Added menu functionality

--- lib/nomad_adapter.rb
+++ lib/nomad_adapter.rb
@@ -2,17 +2,22 @@ require 'pry'
 
 class NomadAdapter
 
+
   attr_reader :query
+  attr_accessor :response, :data
   def initialize(query)
     @query = query.to_s
+    @response = RestClient.get('https://nomadlist.com/api/v2/list/cities')
+    @data = JSON.parse(response)
   end
+  #
+  # def list_cities
+  #   data["result"].collect {|x| x["info"]["city"]["name"]}
+  # end
 
   def search_hash
-    response = RestClient.get('https://nomadlist.com/api/v2/list/cities')
-    data = JSON.parse(response)
     result = data["result"].detect do | city |
       city["info"]["city"]["name"] == @query
     end
   end
-
 end
--- lib/nomad_cli.rb
+++ lib/nomad_cli.rb
@@ -1,14 +1,37 @@
 class NomadCLI
+
+  attr_accessor :flag
+  @flag = false
+
   def call
     welcome
   end
 
   def welcome
     puts "Hi! I'm Ask Travel Jeeves. I know everything about cities. EVERYTHING."
-    puts "Enter a city name to get started: "
+    puts "Enter \'chart\' for top 10 charts, \'search\' to search for a city
+    or \'exit\' to exit."
+    choice
+  end
+
+  def choice
     user_input = gets.strip
-    city = get_city(user_input)
-    main(city)
+
+    if user_input == "chart"
+      # enter chart menu
+    elsif user_input == "search"
+      puts "Enter your city:"
+      city_query = gets.strip
+      city = get_city(city_query)
+      main(city)
+    elsif user_input == "help"
+      # help method
+    elsif user_input == "exit"
+      puts "Goodbye"
+    else
+      puts "Invalid option. Try harder."
+      welcome
+    end
   end
 
   def get_city(user_input)
@@ -16,27 +39,44 @@ class NomadCLI
   end
 
   def main(city)
-    puts "Welcome to #{city.city_name}, #{city.country_name}!"
-    puts "Choose an option from the menu to get started."
-    puts "MENU: \n =========="
-    puts "    1. Does my city have legal weed?"
-    puts "    2. What's my city's average Airbnb price?"
-    puts "    3. What's the weather like right now?"
-    puts "    4. What's the average internet speed?"
-    puts "=========="
-    print "Choose 1, 2, 3 or 4: "
-    user_input = gets.strip
-    if user_input.to_s == "1"
-      city.weed?
-    elsif user_input.to_s == "2"
-      city.av_airbnb
-    elsif user_input.to_s == "3"
-      city.weather
-    elsif user_input.to_s == "4"
-      city.inet_speed
+
+    if !flag
+      puts "\nWelcome to #{city.city_name}, #{city.country_name}!"
+      puts "\nChoose an option from the menu to get started."
     else
-      puts "PLEASE PUT IN A VALID OPTION"
+      puts "\nEnter another choice or enter \'exit\' to exit."
     end
+      puts "\nMENU:\n=========="
+      puts "    1. Does my city have legal weed?"
+      puts "    2. What's my city's average Airbnb price?"
+      puts "    3. What's the weather like right now?"
+      puts "    4. What's the average internet speed?"
+      puts "=========="
+      print "Choose 1, 2, 3 or 4, or exit: "
+    choice_runner(city)
+
+  end
+
+  def choice_runner(city)
+    user_input = gets.strip
+
+    until user_input == "exit"
+      if user_input.to_s == "1"
+        city.weed?
+      elsif user_input.to_s == "2"
+        city.av_airbnb
+      elsif user_input.to_s == "3"
+        city.weather
+      elsif user_input.to_s == "4"
+        city.inet_speed
+      else
+        puts "PLEASE PUT IN A VALID OPTION"
+      end
+      @flag = true
+      main(city)
+    end
+    binding.pry
+    puts "Goodbye."
   end
 
 
